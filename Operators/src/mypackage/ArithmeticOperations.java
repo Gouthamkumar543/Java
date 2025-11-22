b19645199394b3d6e6843992f2a8e9f1d21690d6
Gouthamkumar543 <gouthamkumar0345@gmail.com>
2025-11-22 19:51:30 +0530

    Done operators and Twodimarray

---
+++ Operators/src/mypackage/ArithmeticOperations.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class ArithmeticOperations {
+	public static void main(String []args) {
+		int a = 10, b = 40;
+		System.out.println("Adding two numbers = "+(a+b));
+		System.out.println("subtract two numbers = "+(a-b));
+		System.out.println("divide two numbers = "+(a/b));
+		System.out.println("multiply two numbers = "+(a*b));
+	}
+}
---
+++ Operators/src/mypackage/ComparingOperators.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class ComparingOperators {
+
+	public static void main(String[] args) {
+		int a =10 , b= 20;
+		System.out.println("equal: " + (a==b));
+		System.out.println("lessthan: " + (a<b));
+		System.out.println("greater than: " + (a>b));
+		System.out.println("lessthan or equal: " + (a<=b));
+		System.out.println("greater or equal: " + (a>=b));
+	}
+	
+}
